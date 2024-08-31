79be9edf3825259ed1c6561ffcf6f5fdc56652ba
Niklas Hanft (mointech) <nh@mointech.de>
2024-08-31 21:36:51 +0200

    Mark <chart> as deprecated in favor of package.json

--- @types/k8x/index.d.ts
+++ @types/k8x/index.d.ts
@@ -8,95 +8,127 @@
 // That means we need to declare the JSX namespace but ALSO a global variable that just returns stuff
 declare var React: any;
 
+declare namespace k8x {
+
+  type Chart = {
+    /**
+     * This is the name of the chart
+     */
+    name: string;
+    /**
+     * This is the version of the chart
+     */
+    version: string;
+    /**
+     * This is the version of contained app
+     */
+    appVersion?: string;
+    /**
+     * This is the version of the k8s cluster
+     */
+    kubeVersion?: string;
+    description?: string;
+    type?: string;
+    keywords?: string[];
+    home?: URL;
+    sources?: string[];
+
+    maintainers?: string[];
+    icon?: URL;
+    deprecated?: boolean;
+    annotations?: string[];
+    //children: IntrinsicElements["k8s"];
+  };
+
+  type Cluster = {
+    config?: string;
+    //children?: IntrinsicElements["namespace"];
+  };
+  type Namespace = {
+    name?: string;
+    //children?: (
+    //  | JSX.IntrinsicElements["metadata"]
+    //  | JSX.IntrinsicElements["metadata"]
+    //)[];
+  };
+  type Ingress = {
+    //children: JSX.IntrinsicElements["metadata"];
+  };
+  type Deployment = {
+    "config-path"?: string;
+    //children?: IntrinsicElements["namespace"];
+  };
+  type MatchLabel = {
+    key: string;
+  };
+  type MatchExpression = {
+    key: string;
+    operator: "In" | "Out";
+    values: string[];
+  };
+  type Selector = {}
+  type Spec = {
+    replicas?: number;
+  };
+  type Template = {}
+  type Container = {
+    image: string;
+    imagePullPolicy: "Always" | "Never";
+    name: string;
+  };
+  type Metadata = {
+    name?: string;
+    //children?: JSX.IntrinsicElements["annotation"][];
+  };
+  type Annotation = { [key: string]: string };
+  type Label = Annotation
+  type Service = {
+    kind?: "Service";
+    apiVersion?: "v1";
+  };
+  type Port = {
+    name?: string;
+    protocol: "TCP" | "UDP";
+    port: number;
+    targetPort: number;
+  };
+
+}
+
 declare namespace JSX {
   interface IntrinsicElements {
-    chart: {
-      /**
-       * This is the name of the chart
-       */
-      name: string;
-      /**
-       * This is the version of the chart
-       */
-      version: string;
-      /**
-       * This is the version of contained app
-       */
-      appVersion?: string;
-      /**
-       * This is the version of the k8s cluster
-       */
-      kubeVersion?: string;
-      description?: string;
-      type?: string;
-      keywords?: string[];
-      home?: URL;
-      sources?: string[];
-
-      maintainers?: string[];
-      icon?: URL;
-      deprecated?: boolean;
-      annotations?: string[];
-      //children: IntrinsicElements["k8s"];
-    };
+    /**
+     * Root tag for a k8s application chart
+     * 
+     * @deprecated Use package.json instead
+     * 
+     * @example
+     * export default () => (
+     *   <chart name="wordpress" version="1.0.0" appVersion="6.6.1"></chart>
+     * )
+     */
+    chart: k8x.Chart
     /**
      * @description Use the <cluster> tag to deploy to one or more namespaces at the same time
      * @optional
      */
-    cluster: {
-      config?: string;
-      //children?: IntrinsicElements["namespace"];
-    };
+    cluster: k8x.Cluster
     /*
      * OPTIONAL: Use the <namespace> tag to deploy to one or more namespaces at the same time
      */
-    namespace: {
-      name?: string;
-      //children?: (
-      //  | JSX.IntrinsicElements["metadata"]
-      //  | JSX.IntrinsicElements["metadata"]
-      //)[];
-    };
-    ingress: {
-      //children: JSX.IntrinsicElements["metadata"];
-    };
-    deployment: {
-      "config-path"?: string;
-      //children?: IntrinsicElements["namespace"];
-    };
-    "match-label"?: {
-      key: string;
-    };
-    "match-expression"?: {
-      key: string;
-      operator: "In" | "Out";
-      values: string[];
-    };
-    selector: {};
-    spec: {
-      replicas?: number;
-    };
-    template: {};
-    container: {
-      image: string;
-      imagePullPolicy: "Always" | "Never";
-      name: string;
-    };
-    metadata: {
-      name?: string;
-      //children?: JSX.IntrinsicElements["annotation"][];
-    };
-    annotation: { [key: string]: string };
-    label: { [key: string]: string };
-    service: {
-      kind?: "Service";
-      apiVersion?: "v1";
-    };
-    port: {
-      name?: string;
-      protocol: "TCP" | "UDP";
-      port: number;
-      targetPort: number;
-    };
+    namespace: k8x.Namespace
+    ingress: k8x.Ingress
+    deployment: k8x.Deployment
+    "match-label"?: k8x.MatchLabel
+    "match-expression"?: k8x.MatchExpression
+    selector: k8x.Selector
+    spec: k8x.Spec
+    template: k8x.Template
+    container: k8x.Container
+    metadata: k8x.Metadata
+    annotation: k8x.Annotation
+    label: k8x.Label
+    service: k8x.Service
+    port: k8x.Port
   }
 }
--- examples/wordpress/chart.tsx
+++ examples/wordpress/chart.tsx
@@ -1,44 +1,38 @@
 const replicas = Number(process.env["VARIABLE"]);
 
 export default () => (
-  <chart appVersion="0.0.1" name="Wordpress" version="1.0.0">
-    <cluster config="~/.kube/config">
-      <namespace name="default">
-        <deployment>
-          <spec replicas={replicas}>
-            <selector>
-              <match-label key="appp">snowflake</match-label>
-              <match-expression
-                key="component"
-                operator="In"
-                values={["cache"]}
-              >
-                redis
-              </match-expression>
-            </selector>
-          </spec>
+  <cluster config="~/.kube/config">
+    <namespace name="default">
+      <deployment>
+        <spec replicas={replicas}>
+          <selector>
+            <match-label key="appp">snowflake</match-label>
+            <match-expression key="component" operator="In" values={["cache"]}>
+              redis
+            </match-expression>
+          </selector>
+        </spec>
 
-          <template>
-            <spec>
-              <container
-                image="registry.k8s.io/serve_hostname"
-                imagePullPolicy="Always"
-                name="snowflake"
-              ></container>
-            </spec>
-          </template>
-        </deployment>
-
-        <service>
+        <template>
           <spec>
-            <selector>
-              <match-label key="app.kubernetes.io/name">MyApp</match-label>
-            </selector>
-
-            <port protocol="TCP" port={80} targetPort={80} />
+            <container
+              image="registry.k8s.io/serve_hostname"
+              imagePullPolicy="Always"
+              name="snowflake"
+            ></container>
           </spec>
-        </service>
-      </namespace>
-    </cluster>
-  </chart>
+        </template>
+      </deployment>
+
+      <service>
+        <spec>
+          <selector>
+            <match-label key="app.kubernetes.io/name">MyApp</match-label>
+          </selector>
+
+          <port protocol="TCP" port={80} targetPort={80} />
+        </spec>
+      </service>
+    </namespace>
+  </cluster>
 );
